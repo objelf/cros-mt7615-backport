8d0e80953c6572a0b7841a375708773c538f8d08
Felix Fietkau <nbd@nbd.name>
2020-03-07 12:29:08 +0100

    add missing backports for pci.c
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- v4.14/files/drivers/net/wireless/mt76/hdrs/mt76-chrome.h
+++ v4.14/files/drivers/net/wireless/mt76/hdrs/mt76-chrome.h
@@ -32,6 +32,7 @@
 #include <linux/hrtimer.h>
 #include <crypto/algapi.h>
 #include <linux/pci.h>
+#include <linux/pci-aspm.h>
 #include <linux/if_vlan.h>
 #include <linux/overflow.h>
 
@@ -132,4 +133,33 @@ static inline int atomic_fetch_add_unless(atomic_t *v, int a, int u)
 }
 #endif
 #endif /* LINUX_VERSION_IS_LESS(4,19,0) */
+
+#if defined(CONFIG_PCI)
+#if LINUX_VERSION_IS_LESS(5,3,0)
+
+static inline int
+backport_pci_disable_link_state(struct pci_dev *pdev, int state)
+{
+	u16 aspmc;
+
+	pci_disable_link_state(pdev, state);
+
+	pcie_capability_read_word(pdev, PCI_EXP_LNKCTL, &aspmc);
+	if ((state & PCIE_LINK_STATE_L0S) &&
+	    (aspmc & PCI_EXP_LNKCTL_ASPM_L0S))
+		return -EPERM;
+
+	if ((state & PCIE_LINK_STATE_L1) &&
+	    (aspmc & PCI_EXP_LNKCTL_ASPM_L1))
+		return -EPERM;
+
+	return 0;
+}
+#define pci_disable_link_state backport_pci_disable_link_state
+
+#endif /* < 5.3 */
+#endif /* defined(CONFIG_PCI) */
+
+
+
 #endif /* __MT76_CHROME */
